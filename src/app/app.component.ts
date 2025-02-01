a60b7676098b97cd16fa261acac60e4c4d18f8fb
Boudaya Yassin <boudayayacin007@gmail.com>
2025-02-01 18:32:34 +0100

    add header components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router'; 
 import { AjoutComponent } from './ajout/ajout.component';
 import { ListComponent } from './list/list.component';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  imports: [AjoutComponent,ListComponent],  
+  imports: [AjoutComponent,ListComponent,HeaderComponent],  
 })
 export class AppComponent {
   title = 'testing';
