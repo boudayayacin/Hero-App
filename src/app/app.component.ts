393ce55f1684922a5ec10604db5d84fe4728a051
Boudaya Yassin <boudayayacin007@gmail.com>
2025-02-01 18:19:15 +0100

    move to app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router'; 
-import { FirstComponent } from './first/first.component';
+import { AjoutComponent } from './ajout/ajout.component';
+import { ListComponent } from './list/list.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  imports: [FirstComponent],  
+  imports: [AjoutComponent,ListComponent],  
 })
 export class AppComponent {
   title = 'testing';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';  
 import { AppComponent } from './app.component';
-import { FirstComponent } from './first/first.component';
 import { FormsModule } from '@angular/forms';
-import { AjoutComponent } from './ajout/ajout.component';
-import { ListComponent } from './list/list.component';
+
 
 @NgModule({
   declarations : [
     AppComponent,
-    FirstComponent,
-    AjoutComponent,
-    ListComponent,
   ],
   imports: [BrowserModule, RouterModule , FormsModule], 
   providers: [],
