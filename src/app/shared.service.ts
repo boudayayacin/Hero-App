88239deeb0fdd4a313527c3e7aeda3d36cd5ea10
Boudaya Yassin <boudayayacin007@gmail.com>
2025-02-02 22:51:27 +0100

    add not found

--- src/app/shared.service.ts
+++ src/app/shared.service.ts
@@ -1,10 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SharedService {
-  constructor() { }
+  url = 'http://127.0.0.1:3000/'
+  constructor(private http: HttpClient) { }
   
   heros: any[] = []
+
+  createHero(hero: any){
+    return this.http.post(this.url + 'hero/ajout',hero) ; 
+  }
+
+  getHero(hero: any){
+    return this.http.get(this.url + 'hero/all', hero);
+  }
 }
