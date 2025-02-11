7da4b6a66ef0883a1bb29d8d4bb8492d2dca4f0b
Boudaya Yassin <boudayayacin007@gmail.com>
2025-02-11 19:02:39 +0100

    add logique

--- src/app/devis/devis.component.ts
+++ src/app/devis/devis.component.ts
@@ -1,11 +1,54 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { ServiceService } from '../service.service';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-devis',
-  imports: [],
+  standalone: true, // Ajouté pour permettre l'import
+  imports: [CommonModule, FormsModule, RouterModule],
   templateUrl: './devis.component.html',
-  styleUrl: './devis.component.css'
+  styleUrls: ['./devis.component.css'] // Correction ici (styleUrls avec un "s")
 })
-export class DevisComponent {
+export class DevisComponent implements OnInit {
+  selectedServices: any[] = [];
+  services: any 
+  id: string = "0";
 
+  constructor(public _serv: ServiceService) {}
+
+  ngOnInit(): void {
+    this._serv.getAllservice().subscribe(
+      res => {
+        console.log("Services récupérés :", res);
+        this.services = res;
+      },
+      err => {
+        console.error("Erreur lors de la récupération des services", err);
+      }
+    );
+  }
+
+  onServiceChange(event: Event) {
+    const target = event.target as HTMLSelectElement;
+    const selectedId = target.value;
+
+    if (!selectedId || selectedId === "0") return; 
+
+    if (!this.selectedServices.some(s => s.id === selectedId)) {
+      const service = this.services.find((s: any) => s.id == selectedId);
+      if (service) {
+        this.selectedServices.push({ ...service });
+        console.log("Service ajouté :", service);
+      } else {
+        console.warn("Service non trouvé !");
+      }
+    }
+  }
+
+  removeService(index: number) {
+    this.selectedServices.splice(index, 1);
+    console.log("Service supprimé, index :", index);
+  }
 }
--- src/app/service.service.ts
+++ src/app/service.service.ts
@@ -1,9 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServiceService {
+  constructor(private http: HttpClient) { }
+   url = 'http://127.0.0.1:3000/'
 
-  constructor() { }
+   getAllservice(){
+    return this.http.get(this.url + 'service/all')
+   }
+
 }
